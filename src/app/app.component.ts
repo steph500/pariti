29004d977b9c8030761ff732be29f130999c15ad
steph juma <stephenjuma500@gmail.com>
2022-08-20 14:53:29 +0300

    added checking change function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { ShopItemsInitialData } from './app.response.request';
 export class AppComponent {
 
   title = 'pariti';
+  number: number = 0;
 
   constructor(
   ) { 
@@ -33,29 +34,28 @@ export class AppComponent {
   // dollar denominations for the vending machine
   dollarDenominations = [1, 2, 5, 10, 20, 50, 100];
 
-  // amount of money in the vending machine for each denomination
-  amount = { 1: 0, 2: 0, 5: 0, 10: 0, 20: 0, 50: 0, 100: 0 };
+  // amountInMachine of money in the vending machine for each denomination with type defination
+  amountInMachine: { [key: number]: number } = { 1: 0, 2: 0, 5: 0, 10: 0, 20: 0, 50: 0, 100: 0 };
 
-  // total amount of money available in the vending machine
-  totalAmount = this.amount[1] * 1 + this.amount[2] * 2 + this.amount[5] * 5 +
-    this.amount[10] * 10 + this.amount[20] * 20 + this.amount[50] * 50 + this.amount[100] * 100;
+  // amountInMachine: { [key: number]: number } = {};
+  // total amountInMachine of money available in the vending machine
+  totalAmount = this.amountInMachine[1] * 1 + this.amountInMachine[2] * 2 + this.amountInMachine[5] * 5 +
+    this.amountInMachine[10] * 10 + this.amountInMachine[20] * 20 + this.amountInMachine[50] * 50 + this.amountInMachine[100] * 100;
 
-  amountInserted = { 1: 0, 2: 0, 5: 0, 10: 0, 20: 0, 50: 0, 100: 0 };
+  amountInserted: { [key: number]: number } = { 1: 0, 2: 0, 5: 0, 10: 0, 20: 0, 50: 0, 100: 0 };
   totalAmountInserted = this.amountInserted[1] * 1 + this.amountInserted[2] * 2 +
     this.amountInserted[5] * 5 + this.amountInserted[10] * 10 + this.amountInserted[20] * 20
     + this.amountInserted[50] * 50 + this.amountInserted[100] * 100;
 
-  change = { 1: 0, 2: 0, 5: 0, 10: 0, 20: 0, 50: 0, 100: 0 };
+  change: { [key: number]: number } = { 1: 0, 2: 0, 5: 0, 10: 0, 20: 0, 50: 0, 100: 0 };
   totalChange = this.change[1] * 1 + this.change[2] * 2 + this.change[5] * 5 +
     this.change[10] * 10 + this.change[20] * 20 + this.change[50] * 50 + this.change[100] * 100;
 
 
-  balance = 0;
-
   // function to add selected items to selectedItems array
   addSelectedItem(item: ShopItemsInitialData) {
 
-    // check that the amount of money inserted is enough to buy the item
+    // check that the amountInMachine of money inserted is enough to buy the item
     this.totalAmount = item.price + this.totalAmount;
 
     // update qantity of item in the vending machine
@@ -63,7 +63,6 @@ export class AppComponent {
       if (this.totalAmountInserted >= this.totalAmount) {
         this.selectedItems.push(item);
         item.quantity--;
-        this.balance = this.totalAmountInserted - item.price;
       } else {
         alert('Not enough money to purchase more items');
       }
@@ -72,21 +71,57 @@ export class AppComponent {
     }
   }
 
-  // function to insert random amount of money into the vending machine
+  // function to insert random amountInMachine of money into the vending machine
   insertMoney() {
-    this.amount[1] = Math.floor(Math.random() * 10);
-    this.amount[2] = Math.floor(Math.random() * 10);
-    this.amount[5] = Math.floor(Math.random() * 10);
-    this.amount[10] = Math.floor(Math.random() * 10);
-    this.amount[20] = Math.floor(Math.random() * 10);
-    this.amount[50] = Math.floor(Math.random() * 10);
-    this.amount[100] = Math.floor(Math.random() * 10);
-    this.totalAmountInserted = this.amount[1] * 1 + this.amount[2] * 2 + this.amount[5] * 5 +
-      this.amount[10] * 10 + this.amount[20] * 20 + this.amount[50] * 50 + this.amount[100] * 100;
+    this.amountInMachine[1] = Math.floor(Math.random() * 10);
+    this.amountInMachine[2] = Math.floor(Math.random() * 10);
+    this.amountInMachine[5] = Math.floor(Math.random() * 10);
+    this.amountInMachine[10] = Math.floor(Math.random() * 10);
+    this.amountInMachine[20] = Math.floor(Math.random() * 10);
+    this.amountInMachine[50] = Math.floor(Math.random() * 10);
+    this.amountInMachine[100] = Math.floor(Math.random() * 10);
+    this.totalAmountInserted = this.amountInMachine[1] * 1 + this.amountInMachine[2] * 2 + this.amountInMachine[5] * 5 +
+      this.amountInMachine[10] * 10 + this.amountInMachine[20] * 20 + this.amountInMachine[50] * 50 + this.amountInMachine[100] * 100;
   }
 
   // function to allow user to buy an item
   buyItems() {
-    // check if the amount inserted is enough to buy the item
+    this.totalChange = this.totalAmountInserted - this.totalAmount;
+
+    // this.totalChange = 1564;
+    console.log(this.totalChange)
+    // console.log(this.totalAmountInserted)
+    console.log(this.amountInMachine);
+
+      for (let i = this.dollarDenominations.length; i > 0; i--) {
+        if (this.amountInMachine[this.dollarDenominations[i - 1]] > 0 && this.totalChange >= 
+          this.amountInMachine[this.dollarDenominations[i - 1]]) {
+          var denomination =  Math.floor(this.totalChange / this.dollarDenominations[i - 1]);
+          if (denomination <= this.amountInMachine[this.dollarDenominations[i - 1]]) {
+            this.change[this.dollarDenominations[i - 1]] = denomination;
+
+            this.totalChange = this.totalChange - denomination * this.dollarDenominations[i - 1];
+            this.amountInMachine[this.dollarDenominations[i - 1]] =
+             this.amountInMachine[this.dollarDenominations[i - 1]] - denomination;
+          } else {
+            this.change[this.dollarDenominations[i - 1]] = this.amountInMachine[this.dollarDenominations[i - 1]];
+
+            var difference = denomination - this.amountInMachine[this.dollarDenominations[i - 1]];
+
+            this.totalChange = this.totalChange - this.amountInMachine[this.dollarDenominations[i - 1]] *
+             this.dollarDenominations[i - 1] + difference * this.dollarDenominations[i - 1];
+
+
+            this.amountInMachine[this.dollarDenominations[i - 1]] = 0;
+          }
+
+        } else {
+          console.log(this.change);
+          alert('Not enough money in machine for change');
+        }
+      }
+      console.log(this.change);
+    // reset vending machine
+
   }
 }
