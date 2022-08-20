a3a20ce845a18fbdb79b79d3ebef3922bbf9a64a
steph juma <stephenjuma500@gmail.com>
2022-08-20 18:47:08 +0300

    modified and added more functionality to algorithm for change|

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ShopItemsInitialData } from './app.response.request';
+// import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -43,7 +44,7 @@ export class AppComponent {
     this.amountInMachine[10] * 10 + this.amountInMachine[20] * 20 + this.amountInMachine[50] * 50 + this.amountInMachine[100] * 100;
 
   amountInserted: { [key: number]: number } = { 1: 0, 2: 0, 5: 0, 10: 0, 20: 0, 50: 0, 100: 0 };
-  totalAmountInserted = this.amountInserted[1] * 1 + this.amountInserted[2] * 2 +
+  totalAmountInMachine = this.amountInserted[1] * 1 + this.amountInserted[2] * 2 +
     this.amountInserted[5] * 5 + this.amountInserted[10] * 10 + this.amountInserted[20] * 20
     + this.amountInserted[50] * 50 + this.amountInserted[100] * 100;
 
@@ -54,13 +55,14 @@ export class AppComponent {
 
   // function to add selected items to selectedItems array
   addSelectedItem(item: ShopItemsInitialData) {
-
-    // check that the amountInMachine of money inserted is enough to buy the item
-    this.totalAmount = item.price + this.totalAmount;
-
+    console.log(item.quantity)
     // update qantity of item in the vending machine
-    if(item.quantity) {
-      if (this.totalAmountInserted >= this.totalAmount) {
+
+    //theres a problem here fix
+    fasdf
+    if(item.quantity > 0 ) {
+      if (this.totalAmountInMachine >= this.totalAmount) {
+        this.totalAmount = item.price + this.totalAmount;
         this.selectedItems.push(item);
         item.quantity--;
       } else {
@@ -80,48 +82,60 @@ export class AppComponent {
     this.amountInMachine[20] = Math.floor(Math.random() * 10);
     this.amountInMachine[50] = Math.floor(Math.random() * 10);
     this.amountInMachine[100] = Math.floor(Math.random() * 10);
-    this.totalAmountInserted = this.amountInMachine[1] * 1 + this.amountInMachine[2] * 2 + this.amountInMachine[5] * 5 +
+    this.totalAmountInMachine = this.amountInMachine[1] * 1 + this.amountInMachine[2] * 2 + this.amountInMachine[5] * 5 +
       this.amountInMachine[10] * 10 + this.amountInMachine[20] * 20 + this.amountInMachine[50] * 50 + this.amountInMachine[100] * 100;
+  }
+
+  checkChange() {
+    this.totalChange = this.totalAmountInMachine - this.totalAmount;
+    console.log(this.amountInMachine);
+    console.log(this.totalChange);
+    console.log(this.totalAmountInMachine);
+    console.log(this.totalAmount);
+
+    for (let i = this.dollarDenominations.length; i > 0; i--) {
+      if (this.totalChange >= this.amountInMachine[this.dollarDenominations[i - 1]]) {
+
+        var denomination =  Math.floor(this.totalChange / this.dollarDenominations[i - 1]);
+
+        if (denomination <= this.amountInMachine[this.dollarDenominations[i - 1]]) {
+          this.change[this.dollarDenominations[i - 1]] = denomination;
+
+          this.totalChange = this.totalChange - denomination * this.dollarDenominations[i - 1];
+          this.amountInMachine[this.dollarDenominations[i - 1]] =
+           this.amountInMachine[this.dollarDenominations[i - 1]] - denomination;
+
+        } else { 
+
+          this.change[this.dollarDenominations[i - 1]] = this.amountInMachine[this.dollarDenominations[i - 1]];
+
+          this.totalChange = this.totalChange - this.amountInMachine[this.dollarDenominations[i - 1]] * this.dollarDenominations[i - 1];
+
+          this.amountInMachine[this.dollarDenominations[i - 1]] = 0;
+        }
+
+      }
+    }
+
+    if (this.totalChange) {
+      alert("ERROR: - Money in machine not enough for change, Please contact admin for assistance")
+    }
+    console.log(this.totalChange)
+    console.log(this.amountInMachine);
+    console.log(this.change);
   }
 
   // function to allow user to buy an item
   buyItems() {
-    this.totalChange = this.totalAmountInserted - this.totalAmount;
+    this.checkChange();
 
-    // this.totalChange = 1564;
-    console.log(this.totalChange)
-    // console.log(this.totalAmountInserted)
-    console.log(this.amountInMachine);
-
-      for (let i = this.dollarDenominations.length; i > 0; i--) {
-        if (this.amountInMachine[this.dollarDenominations[i - 1]] > 0 && this.totalChange >= 
-          this.amountInMachine[this.dollarDenominations[i - 1]]) {
-          var denomination =  Math.floor(this.totalChange / this.dollarDenominations[i - 1]);
-          if (denomination <= this.amountInMachine[this.dollarDenominations[i - 1]]) {
-            this.change[this.dollarDenominations[i - 1]] = denomination;
-
-            this.totalChange = this.totalChange - denomination * this.dollarDenominations[i - 1];
-            this.amountInMachine[this.dollarDenominations[i - 1]] =
-             this.amountInMachine[this.dollarDenominations[i - 1]] - denomination;
-          } else {
-            this.change[this.dollarDenominations[i - 1]] = this.amountInMachine[this.dollarDenominations[i - 1]];
-
-            var difference = denomination - this.amountInMachine[this.dollarDenominations[i - 1]];
-
-            this.totalChange = this.totalChange - this.amountInMachine[this.dollarDenominations[i - 1]] *
-             this.dollarDenominations[i - 1] + difference * this.dollarDenominations[i - 1];
-
-
-            this.amountInMachine[this.dollarDenominations[i - 1]] = 0;
-          }
-
-        } else {
-          console.log(this.change);
-          alert('Not enough money in machine for change');
-        }
-      }
-      console.log(this.change);
-    // reset vending machine
-
+    
+    // // post service to buy items
+    // this.selectedItems.forEach(item => {
+    //   this.http.post('http://localhost:3000/items', item).subscribe(res => {
+    //     console.log(res);
+    //   }
+    //   );
+    // }
   }
 }
